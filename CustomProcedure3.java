a3ee85834c1456067a962ee5f9d8857d1cf75839
asmohamed <ahmedsaad0885@gmail.com>
2024-06-25 20:09:18 +0200

    Update CustomProcedure3.java
    fully dynamic

--- CustomProcedure3.java
+++ CustomProcedure3.java
@@ -24,54 +24,94 @@ public class CustomProcedure3 extends AbstractStoredProcedure {
 
     private static final long serialVersionUID = 1L;
     
-  //STATIC METADATA RELATED    
-    Class<?>[] metadata = new Class<?>[] {
-    	Integer.class,
-    	Struct.class,
-    	Timestamp.class,
-    	Boolean.class
-    };
-    Class<?>[] structMetadata = new Class<?>[] {
-        String.class,
-        String.class,
-        BigDecimal.class,
-        String.class, 
-        Timestamp.class,
-        Timestamp.class,
-        String.class,
-        String.class,
-        Integer.class
-    };
-  //STATIC METADATA RELATED
+    List<String> metadata = Arrays.asList(
+            "Integer",
+            "Struct",
+            "Struct",
+            "Struct",
+            "Timestamp",
+            "Boolean"
+        );
 
-    public String getDescription() {
-        return "Get data from JSON file";
-    }
+        List<List<String>> structMetadata = Arrays.asList(
+            Arrays.asList("String",
+                    "String",
+                    "BigDecimal",
+                    "String",
+                    "Timestamp",
+                    "Timestamp",
+                    "String",
+                    "String",
+                    "Integer"
+            ),
+            Arrays.asList(
+                "Integer",
+                
+                "String"
+            ),
+            Arrays.asList(
+                "String",
+                "String",
+                "BigDecimal",
+                "String"
+            )
+        );
 
-    public StoredProcedureParameter[] getParameters() {
-    	//STATIC METADATA RELATED
-        return new StoredProcedureParameter[]{
-            new StoredProcedureParameter("filePath", Types.VARCHAR, StoredProcedureParameter.DIRECTION_IN),
-            new StoredProcedureParameter("financial_claims_release_order_id", Types.INTEGER, StoredProcedureParameter.DIRECTION_OUT),
+        List<List<String>> structNames = Arrays.asList(
+            Arrays.asList(
+                    "description", "fc_reference_number", "amount_in_riyal", "approval_status_arabic_name",
+                    "creation_date", "approval_date", "financial_claim_category_arabic_name",
+                    "financial_claim_sub_category_arabic_name", "randInt"
+            ),
+            Arrays.asList(
+                "attr_a", "attr_c"
+            ),
+            Arrays.asList(
 
-            new StoredProcedureParameter("financial_cliams_list", Types.ARRAY, StoredProcedureParameter.DIRECTION_OUT,
-                true, new StoredProcedureParameter[]{
-                    new StoredProcedureParameter("description", Types.VARCHAR, StoredProcedureParameter.DIRECTION_OUT),
-                    new StoredProcedureParameter("fc_reference_number", Types.VARCHAR, StoredProcedureParameter.DIRECTION_OUT),
-                    new StoredProcedureParameter("amount_in_riyal", Types.DECIMAL, StoredProcedureParameter.DIRECTION_OUT),
-                    new StoredProcedureParameter("approval_status_arabic_name", Types.VARCHAR, StoredProcedureParameter.DIRECTION_OUT),
-                    new StoredProcedureParameter("creation_date", Types.TIMESTAMP, StoredProcedureParameter.DIRECTION_OUT),
-                    new StoredProcedureParameter("approval_date", Types.TIMESTAMP, StoredProcedureParameter.DIRECTION_OUT),
-                    new StoredProcedureParameter("financial_claim_category_arabic_name", Types.VARCHAR, StoredProcedureParameter.DIRECTION_OUT),
-                    new StoredProcedureParameter("financial_claim_sub_category_arabic_name", Types.VARCHAR, StoredProcedureParameter.DIRECTION_OUT),
-                    new StoredProcedureParameter("randInt", Types.INTEGER, StoredProcedureParameter.DIRECTION_OUT)
-                }),
-            new StoredProcedureParameter("last_source_update", Types.TIMESTAMP, StoredProcedureParameter.DIRECTION_OUT),
-            new StoredProcedureParameter("is_from_ro", Types.BOOLEAN, StoredProcedureParameter.DIRECTION_OUT)
-        };
-      //STATIC METADATA RELATED
-    }
-    
+            	    "description", "fc_reference_number", "amount_in_riyal", "approval_status"
+            )
+        );
+
+        public String getDescription() {
+            return "Get data from JSON file";
+        }
+
+        public StoredProcedureParameter[] getParameters() {
+            return new StoredProcedureParameter[]{
+                new StoredProcedureParameter("filePath", Types.VARCHAR, StoredProcedureParameter.DIRECTION_IN),
+                new StoredProcedureParameter("financial_claims_release_order_id", Types.INTEGER, StoredProcedureParameter.DIRECTION_OUT),
+
+                new StoredProcedureParameter("financial_cliams_list", Types.ARRAY, StoredProcedureParameter.DIRECTION_OUT,
+                    true, new StoredProcedureParameter[]{
+                            new StoredProcedureParameter("description", Types.VARCHAR, StoredProcedureParameter.DIRECTION_OUT),
+                            new StoredProcedureParameter("fc_reference_number", Types.VARCHAR, StoredProcedureParameter.DIRECTION_OUT),
+                            new StoredProcedureParameter("amount_in_riyal", Types.DECIMAL, StoredProcedureParameter.DIRECTION_OUT),
+                            new StoredProcedureParameter("approval_status_arabic_name", Types.VARCHAR, StoredProcedureParameter.DIRECTION_OUT),
+                            new StoredProcedureParameter("creation_date", Types.TIMESTAMP, StoredProcedureParameter.DIRECTION_OUT),
+                            new StoredProcedureParameter("approval_date", Types.TIMESTAMP, StoredProcedureParameter.DIRECTION_OUT),
+                            new StoredProcedureParameter("financial_claim_category_arabic_name", Types.VARCHAR, StoredProcedureParameter.DIRECTION_OUT),
+                            new StoredProcedureParameter("financial_claim_sub_category_arabic_name", Types.VARCHAR, StoredProcedureParameter.DIRECTION_OUT),
+                            new StoredProcedureParameter("randInt", Types.INTEGER, StoredProcedureParameter.DIRECTION_OUT)
+                        }),
+
+                new StoredProcedureParameter("financial_cliams_list2", Types.ARRAY, StoredProcedureParameter.DIRECTION_OUT,
+                    true, new StoredProcedureParameter[]{
+                        new StoredProcedureParameter("attr_a", Types.INTEGER, StoredProcedureParameter.DIRECTION_OUT),
+                        new StoredProcedureParameter("attr_c", Types.VARCHAR, StoredProcedureParameter.DIRECTION_OUT)
+                    }),
+
+                new StoredProcedureParameter("financial_cliams_list3", Types.ARRAY, StoredProcedureParameter.DIRECTION_OUT,
+                    true, new StoredProcedureParameter[]{
+                            new StoredProcedureParameter("description", Types.VARCHAR, StoredProcedureParameter.DIRECTION_OUT),
+                            new StoredProcedureParameter("fc_reference_number", Types.VARCHAR, StoredProcedureParameter.DIRECTION_OUT),
+                            new StoredProcedureParameter("amount_in_riyal", Types.DECIMAL, StoredProcedureParameter.DIRECTION_OUT),
+                            new StoredProcedureParameter("approval_status", Types.VARCHAR, StoredProcedureParameter.DIRECTION_OUT)
+                        }),
+
+                new StoredProcedureParameter("last_source_update", Types.TIMESTAMP, StoredProcedureParameter.DIRECTION_OUT),
+                new StoredProcedureParameter("is_from_ro", Types.BOOLEAN, StoredProcedureParameter.DIRECTION_OUT)
+            };
+        }
 
     @Override
     
@@ -83,52 +123,49 @@ public class CustomProcedure3 extends AbstractStoredProcedure {
         
 
         try (JsonParser jsonParser = jsonFactory.createParser(new File(filePath))) {
-        	// START STATIC
-            Integer financialClaimsReleaseOrderId = null;
-            Timestamp lastSourceUpdate = null;
-            Boolean isFromRo = null;
-            
-            //END STATIC
             
 
-            List<Struct> structList = new ArrayList<>();
+        	List<List<Struct>> structLists = new ArrayList<>();
+            for (int i = 0; i < metadata.size(); i++) {
+                structLists.add(new ArrayList<>());
+            }//TODO change
             
             while (jsonParser.nextToken() != JsonToken.END_ARRAY) {
                 if (jsonParser.currentToken() == JsonToken.START_OBJECT) {
-                	int metadataIndex = 0;
-                    while (jsonParser.nextToken() != JsonToken.END_OBJECT && metadataIndex < metadata.length) {
-                        String fieldName = jsonParser.getCurrentName();
+                    int metadataIndex = 0 , structIndex=0 ;
+                    Object[] row = new Object[metadata.size()];
+
+                    while (jsonParser.nextToken() != JsonToken.END_OBJECT && metadataIndex < metadata.size()) {
                         jsonParser.nextToken();
-                        
-                        Class<?> currentType = metadata[metadataIndex++];
-                        
-                        //Start Static
-                        if (currentType == Integer.class) {
-                            financialClaimsReleaseOrderId = handleInt(jsonParser);
-                        } else if (currentType == Struct.class) {
-                            if (jsonParser.currentToken() == JsonToken.START_ARRAY) {
-                                structList = handleStruct(jsonParser, formatter);
-                            }
-                        } else if (currentType == Timestamp.class) {
-                            lastSourceUpdate = handleTimestamp(jsonParser, formatter);
-                        } else if (currentType == Boolean.class) {
-                            isFromRo = handleBoolean(jsonParser);
+
+                        String currentType = metadata.get(metadataIndex);
+                        switch (currentType) {
+                            case "Integer":
+                                row[metadataIndex] = handleInt(jsonParser);
+                                break;
+                            case "Struct":
+                                if (jsonParser.currentToken() == JsonToken.START_ARRAY) {
+                                	 
+                                    List<Struct> structList = handleStruct(jsonParser, formatter, structMetadata.get(structIndex), structNames.get(structIndex));
+									row[metadataIndex] = createArray(structList, Types.STRUCT);
+									structIndex++;
+                                }
+                                break;
+                            case "Timestamp":
+                                row[metadataIndex] = handleTimestamp(jsonParser, formatter);
+                                break;
+                            case "Boolean":
+                                row[metadataIndex] = handleBoolean(jsonParser);
+                                break;
+                            default:
+                                throw new StoredProcedureException("Unsupported type in metadata");
                         }
-                        //END STATIC
+                        metadataIndex++;
                     }
-                metadataIndex=0;
-              //Start Static
-            Object[] row = new Object[metadata.length];
-            row[0] = financialClaimsReleaseOrderId;            
-            row[1] = createArray(structList, Types.STRUCT);
-            row[2] = lastSourceUpdate;
-            row[3] = isFromRo;
-            getProcedureResultSet().addRow(row);
-          //END STATIC
+
+                    getProcedureResultSet().addRow(row);
                 }
             }
-
-
         } catch (IOException e) {
             throw new StoredProcedureException("Error reading JSON file", e);
         }
@@ -179,69 +216,43 @@ public class CustomProcedure3 extends AbstractStoredProcedure {
         }
     }
     
-    private List<Struct> handleStruct(JsonParser jsonParser, DateTimeFormatter formatter){
-    	
+    private List<Struct> handleStruct(JsonParser jsonParser, DateTimeFormatter formatter, List<String> structMetadata, List<String> structNames) {
         List<Struct> structList = new ArrayList<>();
         try {
-			while (jsonParser.nextToken() != JsonToken.END_ARRAY) {
-			    if (jsonParser.currentToken() == JsonToken.START_OBJECT) {
-			    	
-			    	
-			    	int stringIndex = 0, decimalIndex = 0, timestampIndex = 0, intergerIndex = 0;
+            while (jsonParser.nextToken() != JsonToken.END_ARRAY) {
+                if (jsonParser.currentToken() == JsonToken.START_OBJECT) {
+                    List<Object> structValues = new ArrayList<>();
+                    int currentIndex = 0;
+                    while (jsonParser.nextToken() != JsonToken.END_OBJECT) {
+                        jsonParser.nextToken();
 
-			        String[] stringArray = new String[(int) Arrays.stream(structMetadata).filter(type -> type == String.class).count()];
-			        BigDecimal[] decimalArray = new BigDecimal[(int) Arrays.stream(structMetadata).filter(type -> type == BigDecimal.class).count()];
-			        Timestamp[] timestampArray = new Timestamp[(int) Arrays.stream(structMetadata).filter(type -> type == Timestamp.class).count()];
-			        Integer[] integerArray = new Integer[(int) Arrays.stream(structMetadata).filter(type -> type == Integer.class).count()];
-			    	
-			        int currentIndex = 0;
-			        while (jsonParser.nextToken() != JsonToken.END_OBJECT) {
-			            String claimField = jsonParser.getCurrentName();
-			            jsonParser.nextToken();
-
-			            if (structMetadata[currentIndex] == String.class) {
-			                stringArray[stringIndex++] = handleText(jsonParser);
-			            } else if (structMetadata[currentIndex] == BigDecimal.class) {
-			                decimalArray[decimalIndex++] = handleDecimal(jsonParser);
-			            } else if (structMetadata[currentIndex] == Timestamp.class) {
-			                timestampArray[timestampIndex++] = handleTimestamp(jsonParser, formatter);
-			            }else if (structMetadata[currentIndex] == Integer.class) {
-			            	integerArray[intergerIndex++] = handleInt(jsonParser);
-			            }
-			            currentIndex++;
-			        }
-			        List<Object> structValues = new ArrayList<>();
-			        int strIdx = 0, decIdx = 0, tsIdx = 0, intIdx = 0 ;
-			        for (Class<?> type : structMetadata) {
-			            if (type == String.class) {
-			                structValues.add(stringArray[strIdx++]);
-			            } else if (type == BigDecimal.class) {
-			                structValues.add(decimalArray[decIdx++]);
-			            } else if (type == Timestamp.class) {
-			                structValues.add(timestampArray[tsIdx++]);
-			            }else if (type == Integer.class) {
-			            	structValues.add(integerArray[intIdx++]);
-			            }
-			        }
-			        //STATIC METADATA RELATED
-			            Struct struct = super.createStruct(
-			                Arrays.asList(
-			                    "description", "fc_reference_number", "amount_in_riyal", "approval_status_arabic_name",
-			                    "creation_date", "approval_date", "financial_claim_category_arabic_name",
-			                    "financial_claim_sub_category_arabic_name","randInt"
-			                ), structValues);
-			          //STATIC METADATA RELATED
-			            structList.add(struct);
-			        }
-			    }
-		} catch (IOException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		} catch (SQLException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-    	return structList;
+                        String type = structMetadata.get(currentIndex);
+                        switch (type) {
+                            case "String":
+                                structValues.add(handleText(jsonParser));
+                                break;
+                            case "BigDecimal":
+                                structValues.add(handleDecimal(jsonParser));
+                                break;
+                            case "Timestamp":
+                                structValues.add(handleTimestamp(jsonParser, formatter));
+                                break;
+                            case "Integer":
+                                structValues.add(handleInt(jsonParser));
+                                break;
+                            default:
+                                throw new StoredProcedureException("Unsupported type in struct metadata");
+                        }
+                        currentIndex++;
+                    }
+                    Struct struct = super.createStruct(structNames, structValues);
+                    structList.add(struct);
+                }
+            }
+        } catch (IOException | SQLException | StoredProcedureException e) {
+            e.printStackTrace();
+        }
+        return structList;
     }
     
     
